0076d52d03d8c14a263410323e35fd5713af9c64
Tony Kolstee <tony@kolstee.net>
2020-10-25 19:41:38 -0400

    Committed untracked files

---
+++ www/classes/Math.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace classes;
+
+class Math {
+    public function add(array $nums) {
+        return $nums[0] + $nums[1];
+    }
+
+    public function sub(array $nums) {
+        return $nums[0] - $nums[1];
+    }
+
+    public function mul(array $nums) {
+        return $nums[0] * $nums[1];
+    }
+}
