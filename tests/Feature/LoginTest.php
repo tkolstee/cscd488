6b2c12232da6392015fd1de4e546edc7e0150139
Robin <rdeskins17@gmail.com>
2020-11-05 19:20:09 -0800

    Add login/logout feature test

---
+++ tests/Feature/LoginTest.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use App\Models\User;
+use Tests\TestCase;
+
+class LoginTest extends TestCase
+{
+    protected $user;
+    
+    public function testUserCanViewLoginPage()
+    {
+        $response = $this->get('/login');
+        $response->assertStatus(200);
+        $response->assertViewIs('auth.login');
+    }
+
+    public function testAuthenticatedUserCannotViewLoginPage()
+    {
+        $user = User::factory()->make();
+        $response = $this->actingAs($user)->get('/login');
+        $response->assertRedirect('/home');
+    }
+
+    public function testUserCanLoginWithValidCredentials()
+    {
+        $user = User::factory()->create([
+            'password' => bcrypt($password = 'testPass'),
+        ]);
+
+        $response = $this->post('/login', [
+            'email' => $user->email,
+            'password' => $password,
+        ]);
+
+        $response->assertRedirect('/home');
+        $this->assertAuthenticatedAs($user);
+    }
+
+    public function testUserCannotLoginWithInvalidCredentials()
+    {
+        $user = User::factory()->create([
+            'password' => bcrypt($password = 'testPass'),
+        ]);
+
+        $this->post('/login', [
+            'email' => $user->email,
+            'password' => 'wrongPass',
+        ]);
+        $this->assertGuest();
+    }
+
+    public function testUserCanLogout()
+    {
+        $user = User::factory()->create([
+            'password' => bcrypt($password = 'testPass'),
+        ]);
+
+        $this->post('/login', [
+            'email' => $user->email,
+            'password' => $password,
+        ]);
+        $response = $this->post('/logout');
+        $response->assertRedirect('/');
+        $this->assertGuest();
+    }
+}
