faa6cd8c171e57c26ae7577b6a6c99fd3634781c
Steven Zuelke <stevezuelkepsn@gmail.com>
2020-11-09 18:30:40 -0800

    Refactored to factory when applicable

--- tests/Unit/BlueTeamTest.php
+++ tests/Unit/BlueTeamTest.php
@@ -12,6 +12,7 @@ use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Validation\ValidationException;
 use App\Models\User;
 use View;
+use Auth;
 use Exception;
 
 
@@ -47,27 +48,26 @@ class BlueTeamTest extends TestCase
 
     public function testCreateNameAlreadyExists(){
         $this->login();
-        $request = Request::create('/create', 'POST', [
-            'name' => 'test',
-        ]);
+        $team = Team::factory()->make();
+        $team->save();
         $controller = new BlueTeamController();
-        $response = $controller->create($request);
+        $request = Request::create('/create', 'POST', [
+            'name' => $team->name,
+        ]);
         $this->expectException(ValidationException::class);
         $response = $controller->create($request);
     }
 
     public function testDeleteValid(){
         $this->login();
-        $request = Request::create('/create', 'POST', [
-            'name' => 'test',
-        ]);
+        $team = Team::factory()->make();
+        $team->save();
         $controller = new BlueTeamController();
-        $controller->create($request);
         $request = Request::create('/delete', 'POST', [
-            'name' => 'test',
+            'name' => $team->name,
         ]);
         $response = $controller->delete($request);
-        $this->assertTrue(Team::all()->where('name', '=', 'test')->isEmpty());
+        $this->assertTrue(Team::all()->where('name', '=', $team->name)->isEmpty());
     }
 
     public function testDeleteInvalid(){
@@ -78,6 +78,18 @@ class BlueTeamTest extends TestCase
         $controller = new BlueTeamController();
         $this->expectException(Exception::class);
         $response = $controller->delete($request);
+    }
+
+    public function testJoinValid(){
+        $this->login();
+        $controller = new BlueTeamController();
+        $team = Team::factory()->make();
+        $team->save();
+        $request = Request::create('/join', 'POST', [
+            'result' => $team->name,
+        ]);
+        $controller->join($request);
+        $this->assertNotEquals(Auth::user()->blueteam, null);
     }
 
 }
