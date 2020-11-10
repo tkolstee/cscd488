2bc96c9a1e44c327a7c0f92a8e80b91fac1f85c0
Steven Zuelke <stevezuelkepsn@gmail.com>
2020-11-10 13:33:57 -0800

    Fixed Broken Route

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\AssetController::class, 'prefill'])->name('home');
+Route::get('/home', [App\Http\Controllers\AssetController::class, 'prefillTest'])->name('home');
 
 Route::any('/blueteam/{page}', [App\Http\Controllers\BlueTeamController::class, 'page'])->name('blueteam');
 
