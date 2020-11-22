b36675c720d32913c0ebb0329262ad66b6cb80b2
Tony Kolstee <tony@kolstee.net>
2020-11-22 16:14:28 -0500

    Removed accidentally-committed test file

--- test.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$prereqs = ['foo', 'bar'];
-$have    = ['foo', 'one', 'two', 'three'];
-$unmet   = array_diff($prereqs, $have);
-print ("UNMET PREREQS: " );
-print_r($unmet);
-print("\n");
-
