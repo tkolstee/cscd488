84431c66be54cda41feb8ba11a521bfc4b93c7a9
Steven Zuelke <stevezuelkepsn@gmail.com>
2021-01-04 10:22:57 -0800

    Team names instead of numbers

--- resources/views/blueteam/news.blade.php
+++ resources/views/blueteam/news.blade.php
@@ -7,7 +7,7 @@
         <p>No news yet.</p>
     @else
         @foreach ($detectedAttacks as $attack)
-            <p>TeamID #{{$attack->redteam}} attacked TeamID #{{$attack->blueteam}} at {{$attack->updated_at}}</p>
+            <p>Team {{App\Models\Team::find($attack->redteam)->name}} attacked Team {{App\Models\Team::find($attack->blueteam)->name}} at {{$attack->updated_at}}</p>
         @endforeach
         @include('partials.pagination', ['paginator' => $detectedAttacks])
     @endif
