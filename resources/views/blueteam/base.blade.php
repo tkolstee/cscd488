6e045a3ed151b23b8e256558b08e58ed9056e486
Steven Zuelke <stevezuelkepsn@gmail.com>
2021-01-11 15:50:23 -0800

    Your team name is

--- resources/views/blueteam/base.blade.php
+++ resources/views/blueteam/base.blade.php
@@ -43,7 +43,7 @@
                 </div><!--END statsContainer-->
          @else
          <div class="statsContainer">
-                    <div class="statsName"><div class="loggedIn"><p> You are logged in as:</p> </div><div class="loggedInName"> {{  $blueteam->name ?? '' }}</div></div>
+                    <div class="statsName"><div class="loggedIn"><p> Your team name is:</p> </div><div class="loggedInName"> {{  $blueteam->name ?? '' }}</div></div>
                    
                     <div class="stats">Revenue: {{ $blueteam->balance ?? '' }}  |  Reputation: {{ $blueteam->reputation ?? '' }}</div>
                     <div class="stats">Turn: {{ App\Models\Game::turnNumber() }}</div>
