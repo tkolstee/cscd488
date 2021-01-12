4048626b24c4795351e12edc66d440bf64b3b158
Robin <rdeskins17@gmail.com>
2021-01-12 15:40:49 -0800

    news plage displayed created_at, not updated_at

--- resources/views/blueteam/news.blade.php
+++ resources/views/blueteam/news.blade.php
@@ -7,7 +7,7 @@
         <p>No news yet.</p>
     @else
         @foreach ($news as $attack)
-            <p>Team {{App\Models\Team::find($attack->redteam)->name}} attacked Team {{App\Models\Team::find($attack->blueteam)->name}} {{$attack->updated_at->diffForHumans()}}</p>
+            <p>Team {{App\Models\Team::find($attack->redteam)->name}} attacked Team {{App\Models\Team::find($attack->blueteam)->name}} {{$attack->created_at->diffForHumans()}}</p>
         @endforeach
         @include('partials.pagination', ['paginator' => $news])
     @endif
