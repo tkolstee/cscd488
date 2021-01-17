5e1a5ce1d738fc1a6456030cd60f2ba48698b5e0
Robin <rdeskins17@gmail.com>
2021-01-17 14:12:54 -0800

    Fix fillable typo

--- app/Models/User.php
+++ app/Models/User.php
@@ -23,7 +23,7 @@ class User extends Authenticatable
         'email',
         'username',
         'password',
-        'isAdmin',
+        'is_admin',
         'blueteam',
         'leader',
         'redteam',
