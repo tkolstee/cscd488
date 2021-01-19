019ca4c9d6a0a755fd1bfaf0faf984a842fcd71d
Steven Zuelke <stevezuelkepsn@gmail.com>
2021-01-18 17:32:14 -0800

    Fixed upgrading Actions

--- resources/views/partials/inventory_table.blade.php
+++ resources/views/partials/inventory_table.blade.php
@@ -7,26 +7,20 @@
     </thead>
     <tbody>
         @foreach ($inventory ?? [] as $inv)
-        <?php $invAsset = App\Models\Asset::get($inv->asset_name); ?>
+        <?php $invAsset = App\Models\Asset::get($inv->asset_name); 
+        $inArray = in_array("Action",$invAsset->tags)?>
             <tr class="test222">
                 <td ><input type="checkbox" name="results[]" value="{{ $inv->asset_name . $inv->level }}"></td>
                 <td >{{ $invAsset->name }}</td>
                 <td>{{$inv->quantity }}</td>
                 <td>{{$inv->level }}</td>
-                @if($inv->level < 3)
+                @if($inv->level < 3 && (!$inArray))
                     @if($invAsset->blue == 1)    
                         <td><button type="submit" formaction="/blueteam/upgrade" 
                             class="btn btn-primaryInventory" 
                             name="submit" value="{{$invAsset->class_name . $inv->level }}">
                             Upgrade Cost: {{ $inv->getUpgradeCost() }}</button>
                         </td>
-                        @if(in_array("Action",$invAsset->tags))
-                        <td><button type="submit" formaction="/asset" 
-                            class="btn btn-primaryInventory" 
-                            name="submit" value="{{$invAsset->class_name }}">
-                            Use</button>
-                        </td>
-                        @endif
                     @endif
                     @if($invAsset->blue == 0)
                         <td><button type="submit" formaction="/redteam/upgrade" 
@@ -35,7 +29,13 @@
                             Upgrade Cost: {{ $inv->getUpgradeCost() }}</button>
                         </td>
                     @endif
-                    
+                @endif
+                @if($inArray)
+                    <td><button type="submit" formaction="/asset" 
+                        class="btn btn-primaryInventory" 
+                        name="submit" value="{{$invAsset->class_name }}">
+                        Use</button>
+                    </td>
                 @endif
             </tr>
         @endforeach
