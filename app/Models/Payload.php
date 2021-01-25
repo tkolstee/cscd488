9dd1f9b86d56cf7c8712beb1f584e4a816af1a01
Steven Zuelke <stevezuelkepsn@gmail.com>
2021-01-25 13:29:02 -0800

    Set up payload

---
+++ app/Models/Payload.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Exceptions\AssetNotFoundException;
+use Exception;
+
+class Asset //extends Model
+{
+    use HasFactory;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+    ];
+    protected $casts = [ 'rewards' => 'array', ];
+
+    public $_name    = "Abstract class - do not use";
+    public $_rewards = [];
+
+    function __construct() {
+        $this->name           = $this->_name;
+        $this->rewards        = $this->_rewards;
+    }
+    
+    public static function getAll(){
+        $dir = opendir(dirname(__FILE__)."/Payloads");
+        while(($payload = readdir($dir)) !== false){
+            if($payload != "." && $payload != ".."){
+                $length = strlen($payload);
+                $payload = substr($payload, 0, $payload - 4);
+                $class = "\\App\\Models\\Payloads\\" . $payload;
+                $payload[] = new $class();
+            }
+        }
+        if(count($payload) == 0){
+            throw new AssetNotFoundException();
+        }
+        return $payload;
+    }
+
+    public static function get($name){
+        $payloads = Payload::getAll();
+        foreach($payloads as $payload){
+            if($payload->name == $name){
+                return $payload;
+            }
+        }
+        throw new AssetNotFoundException();
+    }
+
+}
---
+++ app/Models/Payloads/EndpointExecutablePayload.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models\Payloads;
+
+use App\Models\Payload;
+
+class EndpointExecutablePayload extends Payload 
+{
+
+    public $_name    = "EndpointExecutable";
+    public $_rewards = ['Ransomware', 'Keylogger'];
+
+}
