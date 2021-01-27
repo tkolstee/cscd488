4acbaccf96f1f433673191efa4457a8bb4f58a90
Robin <rdeskins17@gmail.com>
2021-01-26 17:59:38 -0800

    Fix unit test and percents to be non-decimal

--- app/Models/Payloads/Destruction.php
+++ app/Models/Payloads/Destruction.php
@@ -2,6 +2,8 @@
 
 namespace App\Models\Payloads;
 
+use App\Models\Bonus;
+use App\Models\Team;
 use App\Models\Payload;
 
 class Destruction extends Payload 
@@ -12,5 +14,19 @@ class Destruction extends Payload
 
     public function onAttackComplete($attack){
         parent::onAttackComplete($attack);
+        //Blueteam loses 10% of revenue immediately and lose reputation
+        $blueteam = Team::find($attack->blueteam);
+        $revLost = $blueteam->balance * .10 * -1;
+        $blueteam->changeBalance($revLost);
+        $blueteam->changeReputation($attack->reputation_loss);
+
+        //Blueteam has revenue deduction bonus, starting at 20%
+        $bonus = new Bonus;
+        $bonus->payload_name = $this->_name;
+        $bonus->team_id = $attack->redteam;
+        $bonus->target_id  = $attack->blueteam;
+        $bonus->tags = ['UntilAnalyzed','RevenueDeduction'];
+        $bonus->percentRevDeducted = 20;
+        $bonus->save();
     }
 }
