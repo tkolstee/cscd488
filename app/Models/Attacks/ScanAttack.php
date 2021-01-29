c5fe1e50c20820394068e97c003f06119d2031db
Steven Zuelke <stevezuelkepsn@gmail.com>
2021-01-29 10:37:28 -0800

    Fixed Scan and test

--- app/Models/Attacks/ScanAttack.php
+++ app/Models/Attacks/ScanAttack.php
@@ -4,18 +4,18 @@ namespace App\Models\Attacks;
 
 use App\Models\Attack;
 
-class DosAttack extends Attack {
+class ScanAttack extends Attack {
 
-    public $_name                   = "DoS";
-    public $_class_name             = "Dos";
+    public $_name                   = "Scan";
+    public $_class_name             = "Scan";
     public $_tags                   = [];
     public $_prereqs                = [];
-    public $_payload_choice           = "Dos";
-    public $_initial_difficulty     = 2;
-    public $_initial_detection_risk = 5;
+    public $_payload_choice           = "SecInfo";
+    public $_initial_difficulty     = 1;
+    public $_initial_detection_risk = 2;
     public $_initial_analysis_risk  = 4;
     public $_initial_attribution_risk = 2;
-    public $_initial_energy_cost    = 200;
+    public $_initial_energy_cost    = 20;
     public $_initial_reputation_loss= -100;
     public $possible                = true;
 
