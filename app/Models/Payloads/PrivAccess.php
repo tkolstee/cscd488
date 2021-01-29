7d1a306ee21d244733954d2237a3204976cfe516
Robin <rdeskins17@gmail.com>
2021-01-29 11:22:10 -0800

    Use addToken method in payload methods

--- app/Models/Payloads/PrivAccess.php
+++ app/Models/Payloads/PrivAccess.php
@@ -3,7 +3,7 @@
 namespace App\Models\Payloads;
 
 use App\Models\Payload;
-use App\Models\Inventory;
+use App\Models\Team;
 
 class PrivAccess extends Payload 
 {
@@ -14,11 +14,8 @@ class PrivAccess extends Payload
 
     public function onAttackComplete($attack){
         //Create privileged access token, belonging to redteam. Do not create bonus. 
-        Inventory::create([
-            'quantity' => 1,
-            'team_id' => $attack->redteam,
-            'asset_name' => 'AccessToken',
-            'level' => 2,
-        ]);
+        $redteam = Team::find($attack->redteam);
+        $blueteam = Team::find($attack->blueteam);
+        $redteam->addToken($blueteam->name, 2);
     }
 }
