9f9c3be51773a9716dc76e45de3baf0356e8e527
Robin <rdeskins17@gmail.com>
2021-02-04 12:18:17 -0800

    Add supply chain learn page

--- resources/views/redteam/learn/supplychain.blade.php
+++ resources/views/redteam/learn/supplychain.blade.php
@@ -4,7 +4,7 @@
 
 @section('pagecontent')
 <div class="redLearn">
-    <form method="POST" action="/learn/supplychain">
+    <form method="POST" action="/learn/supplychainhw">
         @csrf
         <input type="hidden" name="step" value="{{ $step }}">
         @if ($step != 1)
@@ -18,24 +18,37 @@
         @endif
 
         @if ($step == 1)
-            <h4>Work In Progress</h4>
-            <p></p>
+            <h4>What is a supply chain attack?</h4>
+            <p>
+                A supply chain attack is when an attacker exploits a weakness <br>
+                in some third-party service the target is using. Often times, the <br>
+                target of the attack is unaware of these vulnerabilities because they <br>
+                did not create the service. Today, many organizations use third-party <br>
+                software, hardware, or services, and these partners are trusted with <br>
+                access to their systems and private data.
+            </p>
 
         @elseif ($step == 2)
-            <h4></h4>
-            <p></p>
+            <h4>Types of supply chain attacks</h4>
+            <p>This game has 3 different types of supply chain attacks:</p>
+            <ul>
+                <li>Dev Tools: Compromise a tool or dependency used by the software development team</li>
+                <li>Hardware: Exploit a weakness in the hardware a company bought from a third party</li>
+                <li>Software: Exploit a weakness in the software a company bought from a third party</li>
+            </ul>
 
         @elseif ($step == 3)
-            <h4></h4>
-            <p></p>
-
-        @elseif ($step == 4)
-            <h4></h4>
-            <p></p>
+            <h4>How can a target prevent supply chain attacks?</h4>
+            <p>
+                Organizations can be more selective with which third-party services they use. For example, <br>
+                in the healthcare sector, many places consider third-party risk and have regulations partners <br>
+                must adhere to. Another possibility is to not use third-party services, but this can be difficult <br>
+                to achieve in the real world. It is often times cheaper to not develop everything "in house". 
+            </p>
 
         @endif
         <!-- Reference:   -->
-        @if ($step != 4)
+        @if ($step != 3)
             <div class="form-group row mb-0">
                 <div class="col-md-8 offset-md-4">
                     <button type="submit" class="btn btn-primary" name="stepChange" value="1">
