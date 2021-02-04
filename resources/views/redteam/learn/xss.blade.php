5d12a6d3f3b05f573f3aca18f04fea3718050c13
Steven Zuelke <stevezuelkepsn@gmail.com>
2021-02-04 13:56:18 -0800

    Step 3

--- resources/views/redteam/learn/xss.blade.php
+++ resources/views/redteam/learn/xss.blade.php
@@ -42,14 +42,35 @@
             <p>This malicious javascript displayed on an unsuspecting user's page <br>
                 will then execute this javascript and send a request to the attackers <br>
                 webpage with the victims cookie as data. This is an example of how <br>
-                attackers can obtain private or sensitive information by XSS.</p>
+                attackers can obtain private or sensitive information by XSS. This <br>
+                is called cookie theft, but there are different kinds like keylogging or phishing.</p>
         @elseif ($step == 2)
-            <h4></h4>
-            <p></p>
-
+            <h4>Types of XSS</h4>
+            <ul>
+                <li>Persistent XSS: the malicious string is from the website's database</li>
+                <li>Reflected XSS: the malicious string is from the user's request</li>
+                <li>DOM-based XSS: the vulnerability is client-side and not server-side</li>
+            </ul><br>
+            <p>The first page went over persistent XSS, where a user submits malicious <br>
+                javascript to the database, and then it is accessed. Reflected XSS seems like <br>
+                it might be useless if a user is attacking itself, but the user is unaware they <br>
+                are submitting a malicious string. This is usually done by an attacker sending <br>
+                out a URL with the malicious string inside of its request. When the user clicks <br>
+                the URL it will submit the malicious string and then execute that string when displayed <br>
+                on the target page.</p>
+            <p>DOM-based XSS is a sort of mix between the two previously described types. When the server-side <br>
+                sends HTML that contains javascript, sometimes the javascript is responsible for displaying content. <br>
+                when this occurs, if the content being displayed is a malicious script, the server-side will <br>
+                be completely unaware. This means the browser executes this malicious script client-side <br>
+                possibly causing them to send a cookie to the attackers website.</p>
         @elseif ($step == 3)
-            <h4></h4>
-            <p></p>
+            <h4>Ways to prevent XSS</h4>
+            <p>In order to be able to display user input without the contained script, <br>
+                proper input/output handling is needed in your program. Two different ways of <br>
+                handling input are encoding and validation. There are a lot of different factors <br>
+                into how it actually gets handled. Depending on where the user input is inserted, <br>
+                the user may insert the closing delimeter for the HTML tag/code in order to further <br>
+                insert malicious javascript.</p>
 
         @elseif ($step == 4)
             <h4></h4>
