0868f8e0c4def8982b277a902a31531f64122182
Robin <rdeskins17@gmail.com>
2021-02-07 13:34:25 -0800

    Add Call Center asset

---
+++ app/Models/Assets/CallCenterAsset.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models\Assets;
+
+use App\Models\Asset;
+
+class CallCenterAsset extends Asset 
+{
+
+    public $_name    = "Call Center";
+    public $_class_name = "CallCenter";
+    public $_tags    = [];
+    public $_blue = 1;
+    public $_buyable = 1;
+    public $_purchase_cost = 2000;
+    public $_ownership_cost = -1500;
+
+    public function onPreAttack($attack)
+    {
+        if (in_array('RequiresUserAction', $attack->tags)){
+            $attack->changeDifficulty(-.2);
+        }
+        if (in_array('PhysicalAttack', $attack->tags)){
+            $attack->changeDifficulty(-.1);
+        }
+    }
+}
