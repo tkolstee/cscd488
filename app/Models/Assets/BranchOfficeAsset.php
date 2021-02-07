74346d523d21c668febec13ccfaf60c73345cca7
Robin <rdeskins17@gmail.com>
2021-02-07 13:37:02 -0800

    Add Branch Office asset

---
+++ app/Models/Assets/BranchOfficeAsset.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models\Assets;
+
+use App\Models\Asset;
+
+class BranchOfficeAsset extends Asset 
+{
+
+    public $_name    = "Branch Office";
+    public $_class_name = "BranchOffice";
+    public $_tags    = [];
+    public $_blue = 1;
+    public $_buyable = 1;
+    public $_purchase_cost = 2000;
+    public $_ownership_cost = -2500;
+
+    public function onPreAttack($attack)
+    {
+        if (in_array('PhysicalAttack', $attack->tags)){
+            $attack->changeDifficulty(-.15);
+        }
+    }
+}
