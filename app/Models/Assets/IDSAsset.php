b286f57e5ae562f38ec5d53b3188c71e826fca7c
Robin <rdeskins17@gmail.com>
2021-02-08 13:18:01 -0800

    Add IDS Asset

---
+++ app/Models/Assets/IDSAsset.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models\Assets;
+
+use App\Models\Asset;
+
+class IDSAsset extends Asset {
+
+    public $_name    = "Intrusion Detection System";
+    public $_class_name = "IDS";
+    public $_tags    = [];
+    public $_blue = 1;
+    public $_buyable = 1;
+    public $_purchase_cost = 200;
+    public $_ownership_cost = 0;
+
+    function onPreAttack($attack) {
+        
+    }
+}
