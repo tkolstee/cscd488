5328051078e0e9f7eca8c50319763497269de97c
Robin <rdeskins17@gmail.com>
2021-02-14 14:17:02 -0800

    Add Datacenter intrusion attack

---
+++ app/Models/Attacks/DataCenterIntrusionAttack.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models\Attacks;
+
+use App\Models\Attack;
+
+class DosAttack extends Attack {
+
+    public $_name                   = "DataCenterIntrusion";
+    public $_class_name             = "Datacenter Intrusion";
+    public $_tags                   = ['PhysicalAttack'];
+    public $_prereqs                = ['PhysicalAccess'];
+    public $_payload_choice           = "Dos";
+    public $_initial_difficulty     = 3.25; //35% chance of success
+    public $_initial_detection_risk = 3.5; //30%
+    public $_initial_analysis_risk  = 1.5; //70%
+    public $_initial_attribution_risk = 3.75; //25%
+    public $_initial_energy_cost    = 200;
+    public $_help_text              = "Perform a physical attack on the target's data center. Requires physical access.";
+
+    public $learn_page              = true;
+
+    function onAttackComplete() {
+        parent::onAttackComplete();
+    }
+
+    function onPreAttack() {
+        parent::onPreAttack();
+    }
+
+}
