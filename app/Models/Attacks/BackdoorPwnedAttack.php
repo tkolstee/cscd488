c1bb607387a26c49c31d4e7f8a77948fae3d7601
Robin <rdeskins17@gmail.com>
2021-02-25 16:31:16 -0800

    Remove lower level token check from BackdoorPwned

--- app/Models/Attacks/BackdoorPwnedAttack.php
+++ app/Models/Attacks/BackdoorPwnedAttack.php
@@ -39,24 +39,6 @@ class BackdoorPwnedAttack extends Attack {
 
     function onPreAttack() {
         parent::onPreAttack();
-        $redteam = Team::find($this->redteam);
-        $blueteam = Team::find($this->blueteam);
-        $tokens = $redteam->getTokens();
-        $lowEnergy = false;
-        $token1 = false;
-        $token2 = false;
-        foreach($tokens as $token){
-            if($token->info ==  $blueteam->name &&  $token->level == 1)
-                $token1 = true;
-            else if($token->info ==  $blueteam->name &&  $token->level == 2)
-                $token2 = true;
-        }
-        if(!$token1 || !$token2){
-            $this->possible = false;
-            $this->detected = false;
-            $this->errormsg = "Missing a lower level access token.";
-        }
-        Attack::updateAttack($this);
         return $this;
     }
 }
